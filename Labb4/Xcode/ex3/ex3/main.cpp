ab241b47710d916714c6b8152e602727034fac20
adrianpalmquist <adrianpalmquist@gmail.com>
2014-12-15 18:11:13 +0100

    added lab4

---
+++ Labb4/Xcode/ex3/ex3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ex3
+//
+//  Created by Adrian Palmquist on 2014-12-15.
+//  Copyright (c) 2014 Adrian Palmquist. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
